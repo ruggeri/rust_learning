6d6b78cfff2d005ccb06a06c297e2b80ab20d1e9
Ned Ruggeri <ruggeri@self-loop.com>
2018-05-20 21:26:02 -1000

    break up code for bst somewhat

--- src/bst/bst.rs
+++ src/bst/bst.rs
@@ -3,21 +3,7 @@
 use std::cmp;
 use std::fmt;
 
-pub struct NodeData<T> {
-  value: T,
-  left: Box<BST<T>>,
-  right: Box<BST<T>>,
-}
-
-impl<T: fmt::Debug> fmt::Debug for NodeData<T> {
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    f.debug_struct("NodeData")
-      .field("value", &self.value)
-      .field("left", &self.left)
-      .field("right", &self.right)
-      .finish()
-  }
-}
+use super::node_data::NodeData;
 
 pub enum BST<T> {
   Nil,
@@ -95,47 +81,6 @@ impl<T: PartialOrd> BST<T> {
       BST::Nil => 0,
       BST::Node(NodeData {box left, box right, ..}) => {
         1 + cmp::max(left.depth(), right.depth())
-      }
-    }
-  }
-}
-
-impl<T: PartialOrd> NodeData<T> {
-  pub fn remove(mut self, t: T) -> BST<T> {
-    match self {
-      _ if t < self.value => {
-        self.left = Box::new(self.left.remove(t));
-        BST::Node(self)
-      },
-      _ if self.value < t => {
-        self.right = Box::new(self.right.remove(t));
-        BST::Node(self)
-      },
-      NodeData { box left, right: box BST::Nil, .. } => {
-        // Nothing on right, so just move up the left.
-        left
-      },
-      NodeData { left: box BST::Nil, box right, .. } => {
-        // Nothing on left, so just move up the right.
-        right
-      }
-      NodeData { left: box BST::Node(left_node_data), .. } => {
-        // Remove the max on the left, and move it into our value.
-        let (new_left, removed_value) = left_node_data.remove_maximum();
-        self.value = removed_value;
-        self.left = Box::new(new_left);
-        BST::Node(self)
-      }
-    }
-  }
-
-  pub fn remove_maximum(mut self) -> (BST<T>, T) {
-    match self.right {
-      box BST::Nil => (*self.left, self.value),
-      box BST::Node(child_data) => {
-        let (new_right, removed_value) = child_data.remove_maximum();
-        self.right = Box::new(new_right);
-        (BST::Node(self), removed_value)
       }
     }
   }
